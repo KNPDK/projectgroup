182abeb4e8229b3fe10c6212e85ee044dcc2460e
Thomas-RC <t-rossi@outlook.com>
2021-02-27 14:42:43 +0100

    Chanege initial

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 
-echo'<h1 style="text-align: center;margin-top: 50px">ProjectGroup KNPDK team</h1>';
+echo'<h1 style="text-align: center;margin-top: 50px; font-weight: bold">ProjectGroup KNPDK team</h1>';
