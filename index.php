66c55a533e62c7254277b81499e0e90f73c1e117
Thomas-RC <t-rossi@outlook.com>
2021-02-28 09:47:46 +0100

    commit 2

--- index.php
+++ index.php
@@ -1,3 +1,3 @@
 <?php
 
-echo'<h2 style="text-align: center;margin-top: 50px; font-weight: bold">ProjectGroup KNPDK team</h2>';
+echo'<h2 style="text-align: center;margin-top: 50px"> ProjectGroup KNPDK team</h2>';
